7e0e78a893bf6fe320b160ce51a5f31f2cdeea6e
nhanph <nhanph@protontech.vn>
2024-12-19 18:06:12 +0700

    up dóc

--- Spring/cache/memory/src/main/java/com/cache/memory/controller/CustomerApi.java
+++ Spring/cache/memory/src/main/java/com/cache/memory/controller/CustomerApi.java
@@ -1,16 +1,11 @@
 package com.cache.memory.controller;
 
-import com.cache.memory.config.dto.PageWrapper;
-import com.cache.memory.entity.Customer;
 import com.cache.memory.service.CustomerService;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class CustomerApi {
